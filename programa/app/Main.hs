b4c9965754f6adfec5594f57573bed5c560b0f07
AndrewLopezHerrera <80350719+AndrewLopezHerrera@users.noreply.github.com>
2024-04-19 21:45:19 -0600

    Se ha fusionado el archivo main

--- programa/app/Main.hs
+++ programa/app/Main.hs
@@ -2,12 +2,9 @@ module Main where
 
 import Datas.Data
 import Inicio.InformacionComercial
-<<<<<<< HEAD
 import Inicio.InformacionBodegas
 import Inicio.InformacionUsuarios
-=======
 import Operaciones.CargarMostrarArticulos
->>>>>>> 698e1a20f01d3c29c4bc73d0e93508f41b4586e8
 
 main :: IO ()
 main = 
@@ -15,15 +12,12 @@ main =
         putStrLn "Bienvenido al Sistema de Inventarios"
         empresa <- cargarDatosEmpresa
         print empresa
-<<<<<<< HEAD
         bodegas <- cargarDatosBodega
         print bodegas
         usuarios <- cargarDatosUsuarios
         print usuarios
-=======
         putStrLn "Ingrese la ruta del archivo de artículos:"
         ruta <- getLine
         articulos <- cargarArticulos ruta
         putStrLn "Artículos cargados:"
         mostrarArticulos articulos
->>>>>>> 698e1a20f01d3c29c4bc73d0e93508f41b4586e8
