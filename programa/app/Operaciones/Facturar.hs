b1b4e88f15d2b27cda94a2f73f6eff290836fdbc
AndrewLopezHerrera <80350719+AndrewLopezHerrera@users.noreply.github.com>
2024-04-24 18:47:12 -0600

    Se comentado el archivo Facturar.hs

--- programa/app/Operaciones/Facturar.hs
+++ programa/app/Operaciones/Facturar.hs
@@ -22,6 +22,10 @@ import Text.Read (readMaybe)
 import Data.Time.Clock.POSIX (getPOSIXTime)
 import Data.ByteString (unpack)
 
+{-
+Entradas: Una factura
+Salidas: Un mensaje de éxito.
+-}
 anadirFactura :: Factura -> IO ()
 anadirFactura nuevaFactura = do
     facturasExistente <- cargarFacturas
@@ -29,6 +33,10 @@ anadirFactura nuevaFactura = do
     guardarFacturas facturasActualizadas
     putStrLn "\nSe ha añadido la factura correctamente."
 
+{-
+Entradas: Nada
+Salidas: La lista de facturas que se encuentran en el archivo JSON.
+-}
 cargarFacturas :: IO [Factura]
 cargarFacturas = do
   cwd <- getCurrentDirectory
@@ -39,6 +47,10 @@ cargarFacturas = do
       return []
     Right factura -> return [factura]
 
+{-
+Entradas: La lista de facturas a guardar.
+Salidas: Un mensjae de éxito.
+-}
 guardarFacturas :: [Factura] -> IO ()
 guardarFacturas facturas = do
   let json = encode facturas
@@ -47,10 +59,17 @@ guardarFacturas facturas = do
   B.writeFile direccion json
   putStrLn "\nSe ha guardado la factura"
 
-
+{-
+Entradas: El código del artículo. La lista de los artículos.
+Salidas: El artículo encontrado
+-}
 buscarArticulo :: String -> [Articulo] -> Maybe Articulo
 buscarArticulo codigo = find (\articulo -> codigoArticulo articulo == codigo)
 
+{-
+Entradas: Una lista de tuplas de bodega e ID. Una lista de tuplas de artículos e ID.
+Salidas: True si hay suficiente stock, False si no lo hay.
+-}
 verificarStock :: [(Bodega, Int)] -> [(Articulo, Int)] -> Bool
 verificarStock _ [] = True
 verificarStock bodegas ((articulo, cantidad):resto) =
@@ -60,6 +79,10 @@ verificarStock bodegas ((articulo, cantidad):resto) =
             in ((stockTotal >= cantidad) && verificarStock bodegas resto)
         Nothing -> False
 
+{-
+Entradas: Una lista de tuplas de bodega e ID. Una lista de tuplas de artículos e ID.
+Salidas: La lista de tuplas bodega e ID actualizado.
+-}
 descontarStock :: [(Bodega, Int)] -> [(Articulo, Int)] -> [(Bodega, Int)]
 descontarStock bodegas [] = bodegas
 descontarStock bodegas ((articulo, cantidad):resto) =
@@ -72,12 +95,24 @@ descontarStock bodegas ((articulo, cantidad):resto) =
         then descontarStock ((bodegaActualizada, stockBodega - cantidadDescontada) : tail bodegas) ((articulo, cantidad - cantidadDescontada) : resto)
         else descontarStock (tail bodegas) ((articulo, cantidad) : resto)
 
+{-
+Entradas: La lista de artículos de la factura.
+Salidas: El subtotal de el precio global.
+-}
 calcularSubtotal :: [ArticuloFactura] -> Double
 calcularSubtotal articulos = sum $ map getSubTotalArticuloFactura articulos
 
+{-
+Entradas: La lista de artículos.
+Salidas: El total de todos los artículos.
+-}
 calcularTotal :: [ArticuloFactura] -> Double
 calcularTotal articulos = sum $ map getTotalArticuloFactura articulos
 
+{-
+Entradas: Una lista de las ordenes de compra. La lista de bodegas. La empresa.
+Salidas: La información de éxito.
+-}
 facturarOrdenCompra :: [OrdenCompra] -> [Bodega] -> Empresa -> IO ()
 facturarOrdenCompra ordenesCompra bodegas empresa = do
     putStr "Ingrese el código de la orden de compra: "
@@ -100,6 +135,11 @@ facturarOrdenCompra ordenesCompra bodegas empresa = do
                 Nothing -> putStrLn "No se encontró ningún usuario asociado a la orden de compra."
         Nothing -> putStrLn "No se encontró ninguna orden de compra con el código especificado."
 
+{-
+Entradas: La orden de compra a facturar. Las bodegas que contienen los artículos. El usuario que
+realizará la facturación. La empresas que realiza la facturación.
+Salidas: La factura creada.
+-}
 facturar :: OrdenCompra -> [Bodega] -> Usuario -> Empresa -> IO (Maybe Factura)
 facturar ordenCompra bodegas usuario empresa = do
     let lineasOrdenCompra = lineasCompra ordenCompra
@@ -127,6 +167,10 @@ facturar ordenCompra bodegas usuario empresa = do
             let total = calcularTotal lineasFactura
             return $ Just $ Factura idFactura usuario empresa "Activo" tiempoActual lineasFactura subtotal total
 
+{-
+Entradas: La factura creada.
+Salidas: La información de la factura.
+-}
 mostrarFactura :: Factura -> IO ()
 mostrarFactura factura = do
     putStrLn "Información de la factura:"
@@ -138,6 +182,10 @@ mostrarFactura factura = do
     putStrLn "Líneas de la factura:"
     mapM_ mostrarLineaFactura (getArticulosFactura factura)
 
+{-
+Entradas: La lista de los artículos de la factura.
+Salidas: La información de cada línea.
+-}
 mostrarLineaFactura :: ArticuloFactura -> IO ()
 mostrarLineaFactura linea = do
     putStrLn $ "Código de artículo: " ++ getCodigoArticuloFactura linea
